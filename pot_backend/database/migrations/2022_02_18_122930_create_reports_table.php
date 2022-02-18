7277e74a4a87057cdfff94f3fd3a70af503a5abe
alux <ahmadabd13741112@gmail.com>
2022-02-18 16:07:49 +0330

    all migrations has been made

---
+++ pot_backend/database/migrations/2022_02_18_122930_create_reports_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('flower_id');
+            $table->foreign('flower_id')->references('id')->on('flowers')->onDelete('cascade');
+            $table->float('height');
+            $table->float('temperature')->default(0);
+            $table->float('humidity')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+};
---
+++ pot_backend/database/migrations/2022_02_18_123443_create_flower_fertilizers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flower_fertilizers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('flower_id');
+            $table->foreign('flower_id')->references('id')->on('flowers')->onDelete('cascade');
+            $table->unsignedBigInteger('fertilizer_id');
+            $table->foreign('fertilizer_id')->references('id')->on('fertilizers')->onDelete('cascade');
+            $table->interger('period');
+            $table->float('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flower_fertilizers');
+    }
+};
